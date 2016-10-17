23e4cece98d411d70ab24b682a341b03be816154
Zhenyuan Li <zl326@afsconnect1.njit.edu>
2016-10-17 18:57:28 -0400

    Third commit

---
+++ IS218HW/index.php
@@ -0,0 +1,26 @@
+<?php
+
+echo "<table>
+ <tr>
+   <th>Firstname</th>
+   <th>Lastname</th>
+   <th>Age</th>
+ </tr>
+ <tr>
+   <td>Jerry</td>
+   <td>Chen</td>
+   <td>60</td>
+ </tr>
+ <tr>
+   <td>Kevin</td>
+   <td>Li</td>
+   <td>80</td>
+ </tr>
+ <tr>
+   <td>Andy</td>
+   <td>Lu</td>
+   <td>100</td>
+ </tr>
+</table>";
+
+?>
